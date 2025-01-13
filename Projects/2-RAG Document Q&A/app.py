bd7c4914077f03eb4d50fe639710166ccc5d1c8a
PrasadLalu <lalumahato020@gmail.com>
2025-01-14 00:14:06 +0200

    RAG Document Q&A part-2

--- Projects/2-RAG Document Q&A/app.py
+++ Projects/2-RAG Document Q&A/app.py
@@ -4,12 +4,10 @@ import streamlit as st
 from dotenv import load_dotenv
 
 from langchain_groq import ChatGroq
-from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_huggingface import HuggingFaceEmbeddings
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -51,6 +49,8 @@ def create_vector_embeddings():
 
 # App title
 st.title("RAG Document Q&A with GROQ and Llama3")
+
+# User input
 user_prompt = st.text_input("Enter your query from the research paper.")
 
 if st.button("Document Embedding"):
@@ -65,7 +65,6 @@ if user_prompt:
     start_time = time.process_time()
     response = retriever_chain.invoke({ "input": user_prompt })
     print(f"Response Time: {time.process_time()-start_time}")
-    
     st.write(response["answer"])
     
     # With a streamlit expander
